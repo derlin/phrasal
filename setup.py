9dc2edf0f0b780ab1b8801f4da39c105449618b6
Lucy Linder <lucy.derlin@gmail.com>
2020-02-17 19:52:28 +0100

    add tests (using tox)
    - rewrite the setup.py script to work with sdist
    - write tests (wip)
    - fix little bugs (thanks to the tests)

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import setuptools
 from os import path
 
-# if you want a requirements.txt as well, use this code and specify install_requires=requirements
-with open(path.join(path.dirname(path.realpath(__file__)), 'requirements.txt'), 'r') as f:
-    requirements = [l.strip() for l in f if len(l) > 0 and not l.isspace()]
-
 setuptools.setup(
     name='phrasal',
     version='0.0.1',
@@ -17,11 +13,11 @@ setuptools.setup(
     # install all packages found under src/
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src', exclude=['showcase']),
-    package_data={'': ['*.yaml', '*.txt']},  # automatically include yaml/txt files
+    #package_data={'': ['*.yaml', '*/*.yaml', '*.txt', '*/*.txt']},  # automatically include yaml/txt files
 
     # include other files such as html, css, etc
     include_package_data=True,  # read from MANIFEST.in
-    zip_safe=True,
+    zip_safe=True, # not safe if the library needs to read package data from os (path is different from a zip)
 
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -30,11 +26,18 @@ setuptools.setup(
     ],
 
     # for testing
-    # setup_requires=['pytest-runner'],
-    # tests_require=['pytest'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-check'],
 
     # regular dependencies
-    install_requires=requirements,
+    install_requires=[
+        'requests>=2.22,<2.23',
+        'regex>=2020.1, <2020.2',
+        'bs4',
+        'justext==2.2.0',
+        'ftfy>=5.6',
+        'pyyaml>=5.3',
+    ],
     # extra dependencies
     extras_require={
         'showcase': ['streamlit']
